de495ea38b3778cbab0bee5123b2d712fe58057c
Yoneo Arai <yoneo.arai@split.io>
2023-06-28 09:13:12 -1000

    Bug fixes
    - interval is now set/reset during listener calls and if the intervalId = null
    - if a tabGroup is clicked/moved (updated), it will be held open for 5 seconds

--- background.js
+++ background.js
@@ -2,11 +2,53 @@
 function minimizeTabGroup(groupId) {
     chrome.tabGroups.update(groupId, { collapsed: true });
 }
+// Helper function to clear the interval function
+function clearIntervalFunction() {
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+    }
+}
+// Helper function to set the interval function
+function setTabGroupInterval() {
+    intervalId = setInterval(function () {
+        chrome.tabs.query({}, function (tabs) {
+            var windowId = tabs[0].windowId; // Assuming all tabs belong to the same window
+            chrome.tabGroups.query({ windowId: windowId }, function (groups) {
+                var _loop_1 = function (group) {
+                    var groupId = group.id;
+                    if (groupId !== activeGroupId &&
+                        !newlyOpenedGroupIds.includes(groupId)) {
+                        chrome.tabs.query({ groupId: groupId }, function (groupTabs) {
+                            var activeTabIds = groupTabs
+                                .filter(function (tab) { return tab.active; })
+                                .map(function (tab) { return tab.id; });
+                            if (activeTabIds.length === 0) {
+                                minimizeTabGroup(groupId);
+                            }
+                        });
+                    }
+                };
+                for (var _i = 0, groups_1 = groups; _i < groups_1.length; _i++) {
+                    var group = groups_1[_i];
+                    _loop_1(group);
+                }
+            });
+        });
+        // Clear newly opened group IDs
+        newlyOpenedGroupIds = [];
+    }, 5000);
+}
 // Store active tab group ID and newly opened group IDs without an active tab
 var activeGroupId = null;
 var newlyOpenedGroupIds = [];
+var intervalId = null;
+// Starts the interval if its not already running
+if (intervalId === null) {
+    setTabGroupInterval();
+}
 // Event listener for tab updates
 chrome.tabs.onActivated.addListener(function (activeInfo) {
+    clearIntervalFunction();
     var tabId = activeInfo.tabId;
     chrome.tabs.get(tabId, function (tab) {
         var groupId = tab.groupId;
@@ -19,18 +61,22 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
             activeGroupId = null;
         }
     });
+    setTabGroupInterval();
 });
 // Event listener for tab creation
 chrome.tabs.onCreated.addListener(function (tab) {
+    clearIntervalFunction();
     var groupId = tab.groupId;
     if (groupId !== -1 &&
         activeGroupId === null &&
         !newlyOpenedGroupIds.includes(groupId)) {
         newlyOpenedGroupIds.push(groupId);
     }
+    setTabGroupInterval();
 });
 // Event listener for new tab creation via link
 chrome.tabs.onCreated.addListener(function (tab) {
+    clearIntervalFunction();
     if (newlyOpenedGroupIds.length === 0 && tab.openerTabId !== undefined) {
         chrome.tabs.get(tab.openerTabId, function (openerTab) {
             var openerGroupId = openerTab.groupId;
@@ -40,32 +86,14 @@ chrome.tabs.onCreated.addListener(function (tab) {
             }
         });
     }
+    setTabGroupInterval();
 });
-// Interval function to close inactive tab groups
-setInterval(function () {
-    chrome.tabs.query({}, function (tabs) {
-        var windowId = tabs[0].windowId; // Assuming all tabs belong to the same window
-        chrome.tabGroups.query({ windowId: windowId }, function (groups) {
-            var _loop_1 = function (group) {
-                var groupId = group.id;
-                if (groupId !== activeGroupId &&
-                    !newlyOpenedGroupIds.includes(groupId)) {
-                    chrome.tabs.query({ groupId: groupId }, function (groupTabs) {
-                        var activeTabIds = groupTabs
-                            .filter(function (tab) { return tab.active; })
-                            .map(function (tab) { return tab.id; });
-                        if (activeTabIds.length === 0) {
-                            minimizeTabGroup(groupId);
-                        }
-                    });
-                }
-            };
-            for (var _i = 0, groups_1 = groups; _i < groups_1.length; _i++) {
-                var group = groups_1[_i];
-                _loop_1(group);
-            }
-        });
-    });
-    // Clear newly opened group IDs
-    newlyOpenedGroupIds = [];
-}, 5000);
+// Event listener for tab group activation
+chrome.tabGroups.onUpdated.addListener(function (activeInfo) {
+    clearIntervalFunction();
+    var groupId = activeInfo.id;
+    if (groupId !== -1) {
+        newlyOpenedGroupIds.push(groupId);
+    }
+    setTabGroupInterval();
+});
--- src/background.ts
+++ src/background.ts
@@ -1,78 +1,109 @@
 // Helper function to minimize a tab group by its groupId
 function minimizeTabGroup(groupId: number) {
-  chrome.tabGroups.update(groupId, { collapsed: true })
+  chrome.tabGroups.update(groupId, { collapsed: true });
+}
+
+// Helper function to clear the interval function
+function clearIntervalFunction() {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+  }
+}
+
+// Helper function to set the interval function
+function setTabGroupInterval() {
+  intervalId = setInterval(function () {
+    chrome.tabs.query({}, function (tabs) {
+      const windowId = tabs[0].windowId; // Assuming all tabs belong to the same window
+      chrome.tabGroups.query({ windowId: windowId }, function (groups) {
+        for (const group of groups) {
+          const groupId = group.id;
+          if (
+            groupId !== activeGroupId &&
+            !newlyOpenedGroupIds.includes(groupId)
+          ) {
+            chrome.tabs.query({ groupId: groupId }, function (groupTabs) {
+              const activeTabIds = groupTabs
+                .filter((tab) => tab.active)
+                .map((tab) => tab.id);
+              if (activeTabIds.length === 0) {
+                minimizeTabGroup(groupId);
+              }
+            });
+          }
+        }
+      });
+    });
+
+    // Clear newly opened group IDs
+    newlyOpenedGroupIds = [];
+  }, 5000);
 }
 
 // Store active tab group ID and newly opened group IDs without an active tab
-let activeGroupId: number | null = null
-let newlyOpenedGroupIds: number[] = []
+let activeGroupId: number | null = null;
+let newlyOpenedGroupIds: number[] = [];
+let intervalId: ReturnType<typeof setInterval> | null = null;
+
+// Starts the interval if its not already running
+if (intervalId === null) {
+  setTabGroupInterval();
+}
 
 // Event listener for tab updates
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-  const tabId = activeInfo.tabId
+  clearIntervalFunction();
+  const tabId = activeInfo.tabId;
   chrome.tabs.get(tabId, function (tab) {
-    const groupId = tab.groupId
+    const groupId = tab.groupId;
     if (groupId !== -1) {
       if (tab.active) {
-        activeGroupId = groupId
+        activeGroupId = groupId;
       }
     } else {
-      activeGroupId = null
+      activeGroupId = null;
     }
-  })
-})
+  });
+  setTabGroupInterval();
+});
 
 // Event listener for tab creation
 chrome.tabs.onCreated.addListener(function (tab) {
-  const groupId = tab.groupId
+  clearIntervalFunction();
+  const groupId = tab.groupId;
   if (
     groupId !== -1 &&
     activeGroupId === null &&
     !newlyOpenedGroupIds.includes(groupId)
   ) {
-    newlyOpenedGroupIds.push(groupId)
+    newlyOpenedGroupIds.push(groupId);
   }
-})
+  setTabGroupInterval();
+});
 
 // Event listener for new tab creation via link
 chrome.tabs.onCreated.addListener(function (tab) {
+  clearIntervalFunction();
   if (newlyOpenedGroupIds.length === 0 && tab.openerTabId !== undefined) {
     chrome.tabs.get(tab.openerTabId, function (openerTab) {
-      const openerGroupId = openerTab.groupId
+      const openerGroupId = openerTab.groupId;
       if (
         openerGroupId !== -1 &&
         !newlyOpenedGroupIds.includes(openerGroupId)
       ) {
-        newlyOpenedGroupIds.push(openerGroupId)
+        newlyOpenedGroupIds.push(openerGroupId);
       }
-    })
+    });
   }
+  setTabGroupInterval();
+});
+
+// Event listener for tab group activation
+chrome.tabGroups.onUpdated.addListener(function (activeInfo) {
+  clearIntervalFunction()
+  const groupId = activeInfo.id
+  if (groupId !== -1) {
+    newlyOpenedGroupIds.push(groupId);
+  }
+  setTabGroupInterval()
 })
-
-// Interval function to close inactive tab groups
-setInterval(function () {
-  chrome.tabs.query({}, function (tabs) {
-    const windowId = tabs[0].windowId // Assuming all tabs belong to the same window
-    chrome.tabGroups.query({ windowId: windowId }, function (groups) {
-      for (const group of groups) {
-        const groupId = group.id
-        if (
-          groupId !== activeGroupId &&
-          !newlyOpenedGroupIds.includes(groupId)
-        ) {
-          chrome.tabs.query({ groupId: groupId }, function (groupTabs) {
-            const activeTabIds = groupTabs
-              .filter(tab => tab.active)
-              .map(tab => tab.id)
-            if (activeTabIds.length === 0) {
-              minimizeTabGroup(groupId)
-            }
-          })
-        }
-      }
-    })
-  })
-
-  // Clear newly opened group IDs
-  newlyOpenedGroupIds = []
-}, 5000)
